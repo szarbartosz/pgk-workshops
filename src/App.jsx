42633d5fa8318fe81c7a9eb3a706546ecbdbdb24
Bartosz Szar <szarbartosz@gmail.com>
2023-12-07 16:34:46 +0100

    remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,9 @@ export default function App() {
   return (
     <Canvas shadows camera={{ position: [30, 30, 30] }}>
       {/* <Environment files="assets/pictures/winter.hdr" ground={{ scale: 100 }} /> */}
-      {/* <Bedroom scale={2} /> */}
       <Lights />
       <Lamp position={[0, 10, 0]} scale={4} />
-      {/* <Dog position={[0, 0, 0]} scale={0.1} /> */}
       <Hospital position={[0, 0, 0]} scale={0.08} />
-      {/* <HospitalShade position={[0, 0, 0]} scale={0.08} /> */}
-      {/* <ambiesntLight intensity={0.2} /> */}
-      {/* <pointLight intensity={0.2} /> */}
       <OrbitControls />
       {/* <Stats /> */}
       {/* <axesHelper /> */}
